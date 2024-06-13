37288f1c8283da0f876c792deaba6466bdd649f2
pedro <pedro.h.avila.v@gmail.com>
2024-06-13 18:15:10 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import tabela2024 from "./tabela";
+import express from 'express';
+
+const server = express();
+
+server.listen(300, () => console.log("servidor rodando com sucesso"))
+;
+
---
+++ tabela.js
@@ -0,0 +1,8 @@
+const time = {
+    nome: 'Cruzeiro',
+    sigla: 'CRU',
+    pontos: 0,
+    vitorias: 0,
+    empates: 0,
+    golsMarcados: 0,
+};
