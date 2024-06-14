11aa433ad0974e9b6ff1dc5b2f09ca21de4dfbf7
pedro <pedro.h.avila.v@gmail.com>
2024-06-13 21:31:54 -0300

    subirdois

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ import express from 'express';
 
 const server = express();
 
+server.get('/',(req,res) => {
+    res.send(tabela2024);
+});
 server.listen(300, () => console.log('servidor rodando com sucesso'));
 
