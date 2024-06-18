bf3215b6a70190b96f0c36ba5a85190198a41811
pedro <pedro.h.avila.v@gmail.com>
2024-06-18 20:20:47 -0300

    receba

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ const server = express();
 server.get('/',(req,res) => {
     res.send(tabela2024);
 });
+
+server.get('/:sigla',(req,res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const time = tabela2024.find(siglaInformada);
+    res.send(time);
+    });
 server.listen(300, () => console.log('servidor rodando com sucesso'));
 
