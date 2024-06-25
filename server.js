0a7cbec3e940a95cad3397935669f2ba541312d1
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2024-06-25 18:25:39 -0300

    get

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ import express from 'express';
 
 const server = express();
 
-server.get('/',(req,res) => {
+server.get('/', (req,res) => {
     res.send(tabela2024);
+});
+
+server.get('/:sigla', (req,res) => {
+
+     const siglaInformada = req.params.sigla.toUpperCase();
+     const time = tabela2024.find((infoTime) => infoTime.sigla === siglaInformada);
+
+    if (!time) {
+        resposta.status(404).send('Não existe na série A do Brasileirão 2024 um time com a sigla informada!');
+        return;
+    }
+    resposta.status(202).send(time);
 });
 server.listen(300, () => console.log('servidor rodando com sucesso'));
 
