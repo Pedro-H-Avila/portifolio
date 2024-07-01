1ac613395cc02121969649de5c59d6b4bf573538
pedro <pedro.h.avila.v@gmail.com>
2024-07-01 19:50:43 -0300

    nossaSenhora

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.get('/:sigla', (req,res) => {
      const time = tabela2024.find((infoTime) => infoTime.sigla === siglaInformada);
 
     if /*(time === undefined) */ (!time) {
-        resposta.status(404).send('Não existe na série A do Brasileirão 2024 um time com a sigla informada!');
+        res.status(404).send('Não existe na série A do Brasileirão 2024 um time com a sigla informada!');
         return;
     }
     res.status(200).send(time);
